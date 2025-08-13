a0bccb044180ed85721761d01b4b634a6a3064eb
thatopayday <40715822+Bethhelemuel@users.noreply.github.com>
2025-08-13 11:04:59 +0200

    Deploy
    Deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'docs', // changes the folder name from .next to next
-  assetPrefix: './', // ensures relative paths for GitHub Pages
+
   output: 'export', // needed if you are using static export
 };
 
