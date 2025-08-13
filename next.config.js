d59d068a6379f3a0c476141e6474f09c8faa9767
thatopayday <40715822+Bethhelemuel@users.noreply.github.com>
2025-08-13 09:15:31 +0200

    deploy
    Deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,           // ensures URLs end with /
-  basePath: '/profile',          // your GitHub repo name
-  assetPrefix: './',             // relative path for _next assets
+  output: 'export', // keep this for static export
 };
+
 module.exports = nextConfig;
